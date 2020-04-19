3ababb49b5bb091fb525793dbfa64010949b4f3d
itzhanglg <1321340821@qq.com>
2020-04-19 16:15:31 +0800

    springboot中使用swagger2

---
+++ springboot-swagger/src/main/java/com/springboot/demo/service/MessageRepository.java
@@ -0,0 +1,22 @@
+
+package com.springboot.demo.service;
+
+import com.springboot.demo.entity.Message;
+
+import java.util.List;
+
+public interface MessageRepository {
+
+	List<Message> findAll();
+
+	Message save(Message message);
+
+	Message update(Message message);
+
+	Message updateText(Message message);
+
+	Message findMessage(Long id);
+
+	void deleteMessage(Long id);
+
+}
